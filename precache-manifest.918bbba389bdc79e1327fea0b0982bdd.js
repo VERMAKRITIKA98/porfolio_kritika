cb9131a13000ac5cf54e5c02910f892c3e05117c
KritikaDigitiz <kritikav@digitizeco.in>
2023-03-01 15:52:17 +0530

    Updates

---
+++ precache-manifest.918bbba389bdc79e1327fea0b0982bdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b606e3205d4c06a751a",
+    "url": "/porfolio_kritika/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "6b606e3205d4c06a751a",
+    "url": "/porfolio_kritika/static/js/main.929d1524.chunk.js"
+  },
+  {
+    "revision": "e66a590951c48566ed49",
+    "url": "/porfolio_kritika/static/js/runtime~main.b79e59be.js"
+  },
+  {
+    "revision": "258e19efbf1bad0d3d74",
+    "url": "/porfolio_kritika/static/js/2.fb14bd91.chunk.js"
+  },
+  {
+    "revision": "5647669883a9ab059f93947d656118c0",
+    "url": "/porfolio_kritika/index.html"
+  }
+];
