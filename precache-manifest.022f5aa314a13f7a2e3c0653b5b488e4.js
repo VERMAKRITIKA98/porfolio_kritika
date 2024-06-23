bb96c820dd7525e5e07d4db27f9518ee2326d03d
Kritika2202 <95027511+Kritika2202@users.noreply.github.com>
2024-06-24 01:20:36 +0530

    Updates

--- precache-manifest.022f5aa314a13f7a2e3c0653b5b488e4.js
+++ precache-manifest.022f5aa314a13f7a2e3c0653b5b488e4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6b606e3205d4c06a751a",
+    "revision": "2f19ceb0702ae8475583",
     "url": "/porfolio_kritika/static/css/main.07318036.chunk.css"
   },
   {
-    "revision": "6b606e3205d4c06a751a",
-    "url": "/porfolio_kritika/static/js/main.929d1524.chunk.js"
+    "revision": "2f19ceb0702ae8475583",
+    "url": "/porfolio_kritika/static/js/main.402ab4cf.chunk.js"
   },
   {
     "revision": "e66a590951c48566ed49",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/porfolio_kritika/static/js/2.fb14bd91.chunk.js"
   },
   {
-    "revision": "5647669883a9ab059f93947d656118c0",
+    "revision": "507f27b43f473f842d5e8778976890fb",
     "url": "/porfolio_kritika/index.html"
   }
 ];
