9b29e3316102ee2e297988dfdea3684d884540a4
VERMAKRITIKA98 <vermakritika98@gmail.com>
2025-09-14 04:32:36 +0530

    updated summary and bio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -37,7 +37,7 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">{name}.</h1>
+            <h1 className="responsive-headline">{name}</h1>
             <h2 className="responsive-headline">I'm a {occupation}</h2>
             
             <h3>
